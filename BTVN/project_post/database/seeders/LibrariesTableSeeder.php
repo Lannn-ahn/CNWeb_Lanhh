95b99e27bed6c765dbe22ef7f71c375c9cb03022
Lannn-ahn <lananhvuhoang7@gmail.com>
2024-12-12 11:08:08 +0700

    btvn

---
+++ BTVN/project_post/database/seeders/LibrariesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class LibrariesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create(); 
+        foreach (range(1, 100) as $index) { 
+            DB::table('libraries')->insert([
+                'name' => $faker->name,
+                'address' => $faker->address,
+                'contact_number' => $faker->numerify('0#########'), 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+} 
